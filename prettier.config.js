0fd4b61c10074a0655b4eab9ed8d0b8ef3e22d1e
Abishek Murali Mohan <abishek.murali.mohan@oracle.com>
2019-08-09 11:03:06 -0700

    Update configs

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * Install the "Prettier - Code formatter" VSCode extension:
+ * https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+ *
+ * Note: Changing to this file might require restarting VSCode for the changes to kick in.
+ */
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  proseWrap: "always",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
---
+++ wiki/favico.md
@@ -0,0 +1,3 @@
+## Resources
+
+https://www.emergeinteractive.com/insights/detail/the-essentials-of-favicons/
